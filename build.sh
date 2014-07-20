103c27eb1a353c287bb1c406ba5fe2de28049b3b
Lu Wang <coolwanglu@gmail.com>
2014-07-20 00:09:37 -0700

    init

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+[ -z $EM_DIR] && EM_DIR=~/src/emscripten
+[ -z $PDCurses_DIR] && PDCurses_DIR=~/src/PDCurses.js
+[ -z $AALib_DIR] && AALib_DIR=~/src/aalib.js
+
+do_config() {
+    echo config
+# something wrong with emcc + cproto, use gcc as CPP instead
+CPPFLAGS="-Os -I$AALib_DIR/src" \
+$EM_DIR/emconfigure ./configure \
+  --without-x \
+
+}
+
+do_make() {
+$EM_DIR/emmake make CFLAGS="-Os -I$AALib_DIR/src" 
+}
+
+do_link() {
+pushd web
+cp ../bb bb.bc 
+$EM_DIR/emcc \
+    bb.bc \
+    $AALib_DIR/src/.libs/libaa.a \
+    $PDCurses_DIR/sdl1/libpdcurses.a \
+    -o bb.html \
+    -Oz \
+    --memory-init-file 1 \
+    -s ASYNCIFY=1 \
+    --preload-file pdcfont.bmp \
+
+popd
+}
+
+do_config
+do_make
+do_link
